232291b3f3f8f6da2de77de5e3d08162b9301d76
vossgang <vossgang@gmail.com>
2014-04-21 09:22:05 -0700

    Initial Commit

---
+++ GithubToGO/MVViewController.m
@@ -0,0 +1,29 @@
+//
+//  MVViewController.m
+//  GithubToGO
+//
+//  Created by Matthew Voss on 4/21/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import "MVViewController.h"
+
+@interface MVViewController ()
+
+@end
+
+@implementation MVViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GithubToGO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GithubToGO
+//
+//  Created by Matthew Voss on 4/21/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MVAppDelegate class]));
+    }
+}
