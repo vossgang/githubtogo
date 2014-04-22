0e974457fcde521f15a14433e99c8d33f225a544
vossgang <vossgang@gmail.com>
2014-04-21 22:17:09 -0700

    first

--- GithubToGO/BurgerProtocal.h
+++ GithubToGO/BurgerProtocal.h
@@ -1,13 +1,13 @@
 //
-//  MVViewController.h
+//  BurgerProtocal.h
 //  GithubToGO
 //
 //  Created by Matthew Voss on 4/21/14.
 //  Copyright (c) 2014 Matthew Voss. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface MVViewController : UIViewController
+@protocol BurgerProtocal <NSObject>
 
 @end
---
+++ GithubToGO/RootMenuController.h
@@ -0,0 +1,13 @@
+//
+//  RootMenuController.h
+//  GithubToGO
+//
+//  Created by Matthew Voss on 4/21/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootMenuController : UIViewController
+
+@end
